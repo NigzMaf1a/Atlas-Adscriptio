3605b0267b4d2dca682a0e6a25d4db4a1e2fdbf8
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-07-29 18:49:31 +0300

    feat:Production ready

--- internal/queries/taskalloc.go
+++ internal/queries/taskalloc.go
@@ -5,10 +5,20 @@ type TaskAllocQueries struct {
 	ReadAllocs  string
 }
 
-var Alloc_Queries TaskAllocQueries = TaskAllocQueries{
-	CreateAlloc: `INSERT INTO task_allocations
-				  (task_id,registration_id)
-				  VALUES ($1,$2)
-	             `,
-	ReadAllocs: ``,
+var Alloc_Queries = TaskAllocQueries{
+	CreateAlloc: `
+		INSERT INTO task_allocations
+		(
+			task_id,
+			registration_id
+		)
+		VALUES ($1,$2)
+		RETURNING task_allocation_id`,
+
+	ReadAllocs: `
+		SELECT
+			task_allocation_id,
+			task_id,
+			registration_id
+		FROM task_allocations`,
 }
--- internal/queries/tasks.go
+++ internal/queries/tasks.go
@@ -6,11 +6,31 @@ type TaskQueries struct {
 	ReadTasks        string
 }
 
-var Task_Queries TaskQueries = TaskQueries{
-	CreateTask: `INSERT INTO tasks
-				 (registration_id,task_detail,task_status,start_time,end_time)
-				 VALUES ($1,$2,$3,$4,$5)
-	           `,
-	UpdateTaskStatus: `UPDATE tasks SET task_status = $1 WHERE id = $2`,
-	ReadTasks:        ``,
+var Task_Queries = TaskQueries{
+	CreateTask: `
+		INSERT INTO tasks
+		(
+			registration_id,
+			task_detail,
+			task_status,
+			start_time,
+			end_time
+		)
+		VALUES ($1,$2,$3,$4,$5)
+		RETURNING task_id`,
+
+	UpdateTaskStatus: `
+		UPDATE tasks
+		SET task_status = $1
+		WHERE task_id = $2`,
+
+	ReadTasks: `
+		SELECT
+			task_id,
+			registration_id,
+			task_detail,
+			task_status,
+			start_time,
+			end_time
+		FROM tasks`,
 }
