9a8d8d3c66a33a9bbf8b1753d35484179bafa316
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-02 16:14:56 +0300

    feat:added handlers for contact CRUD operations

--- internal/handler/contact.go
+++ internal/handler/contact.go
@@ -6,16 +6,16 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/NigzMaf1a/Atlas-Adscriptio/internal/operations/about"
+	"github.com/NigzMaf1a/Atlas-Adscriptio/internal/operations/contacts"
 	"github.com/NigzMaf1a/Atlas-Adscriptio/internal/scripts"
 )
 
-func ReadAbout(db *sql.DB) http.HandlerFunc {
+func ReadContacts(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 		defer cancel()
 
-		about, err := about.GetAbout(ctx, db)
+		contacts, err := contacts.GetContacts(ctx, db)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -23,14 +23,14 @@ func ReadAbout(db *sql.DB) http.HandlerFunc {
 
 		w.Header().Set("Content-Type", "application/json")
 
-		if err := scripts.EncodeJSON(w, about); err != nil {
+		if err := scripts.EncodeJSON(w, contacts); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
 }
 
-func UpdateAbout(db *sql.DB) http.HandlerFunc {
+func UpdateContacts(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
 
@@ -40,7 +40,7 @@ func UpdateAbout(db *sql.DB) http.HandlerFunc {
 			return
 		}
 
-		detail, err := scripts.DecodeJSON[string](r.Body)
+		contact, err := scripts.DecodeJSON[contacts.Contact](r.Body)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -49,7 +49,7 @@ func UpdateAbout(db *sql.DB) http.HandlerFunc {
 		ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 		defer cancel()
 
-		if err := about.UpdateAbout(ctx, db, id, detail); err != nil {
+		if err := contacts.UpdateContact(ctx, db, contact, id); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
